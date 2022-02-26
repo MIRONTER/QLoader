35820f54dd7ecbf58b231720357fb0ab1e9b9d3b
skrimix <28156185+skrimix@users.noreply.github.com>
2022-02-26 20:12:34 +0500

    Separate device settings code

---
+++ QSideloader/ViewModels/DeviceSettingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
+
+namespace QSideloader.ViewModels;
+
+public class DeviceSettingsViewModel : ViewModelBase, IActivatableViewModel
+{
+    public DeviceSettingsViewModel()
+    {
+        Activator = new ViewModelActivator();
+        this.WhenActivated(disposables =>
+        {
+            Disposable
+                .Create(() => { })
+                .DisposeWith(disposables);
+        });
+    }
+    
+    public ViewModelActivator Activator { get; }
+}
---
+++ QSideloader/Views/DeviceSettingsView.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using QSideloader.ViewModels;
+using ReactiveUI;
+
+namespace QSideloader.Views;
+
+public partial class DeviceSettingsView : ReactiveUserControl<DeviceSettingsViewModel>
+{
+    public DeviceSettingsView()
+    {
+        ViewModel = new DeviceSettingsViewModel();
+        DataContext = ViewModel;
+        this.WhenActivated(disposables => { });
+
+        
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
