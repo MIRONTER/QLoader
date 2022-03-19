ddf54fdf49c8824dfe6b480cd848e73a11defb34
skrimix <28156185+skrimix@users.noreply.github.com>
2022-03-19 17:02:51 +0500

    Add search function
    Closes #16

--- QSideloader/ViewModels/AvailableGamesViewModel.cs
+++ QSideloader/ViewModels/AvailableGamesViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.Threading;
+using DynamicData;
 using QSideloader.Helpers;
 using QSideloader.Models;
 using QSideloader.Services;
@@ -18,12 +21,37 @@ public class AvailableGamesViewModel : ViewModelBase, IActivatableViewModel
     private readonly ObservableAsPropertyHelper<bool> _isBusy;
     private readonly AdbService _adbService;
     private readonly DownloaderService _downloaderService;
+    private readonly SourceCache<Game, string> _availableGamesSourceCache = new(x => x.ReleaseName!);
+    private readonly ReadOnlyObservableCollection<Game> _availableGames;
 
     public AvailableGamesViewModel()
     {
         _adbService = ServiceContainer.AdbService;
         _downloaderService = ServiceContainer.DownloaderService;
         Activator = new ViewModelActivator();
+
+        Func<Game, bool> GameFilter(string text) => game => string.IsNullOrEmpty(text)
+                                                            || text.Split(' ')
+                                                                .All(x => game.ReleaseName!.ToLower()
+                                                                    .Contains(x.ToLower()));
+
+        var filterPredicate = this.WhenAnyValue(x => x.SearchText)
+            .Throttle(TimeSpan.FromMilliseconds(250))
+            .DistinctUntilChanged()
+            .Select(GameFilter);
+        var cleanUp = _availableGamesSourceCache.Connect()
+            .RefCount()
+            .ObserveOn(RxApp.TaskpoolScheduler)
+            .Filter(filterPredicate)
+            .SortBy(x => x.ReleaseName!)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Bind(out _availableGames)
+            .DisposeMany()
+            .Subscribe();
+        this.WhenActivated(disposables =>
+        {
+            cleanUp.DisposeWith(disposables);
+        });
         Refresh = ReactiveCommand.CreateFromObservable(RefreshImpl);
         Refresh.IsExecuting.ToProperty(this, x => x.IsBusy, out _isBusy, false, RxApp.MainThreadScheduler);
         Install = ReactiveCommand.CreateFromObservable(InstallImpl);
@@ -32,10 +60,11 @@ public class AvailableGamesViewModel : ViewModelBase, IActivatableViewModel
 
     public ReactiveCommand<Unit, Unit> Refresh { get; }
     public ReactiveCommand<Unit, Unit> Install { get; }
-    [Reactive] public ObservableCollection<Game> AvailableGames { get; set; } = new();
+    public ReadOnlyObservableCollection<Game> AvailableGames => _availableGames;
     public bool IsBusy => _isBusy.Value;
     [Reactive] public bool MultiSelectEnabled { get; set; } = true;
     private bool FirstRefresh { get; set; } = true;
+    [Reactive] public string SearchText { get; set; } = "";
     public ViewModelActivator Activator { get; }
 
     private IObservable<Unit> RefreshImpl()
@@ -88,10 +117,15 @@ public class AvailableGamesViewModel : ViewModelBase, IActivatableViewModel
 
         var toAdd = Globals.AvailableGames.Except(AvailableGames).ToList();
         var toRemove = AvailableGames.Except(Globals.AvailableGames).ToList();
-        foreach (var addedGame in toAdd)
-            AvailableGames.Add(addedGame);
-        foreach (var removedGame in toRemove)
-            AvailableGames.Remove(removedGame);
+        _availableGamesSourceCache.Edit(innerCache =>
+        {
+            innerCache.AddOrUpdate(toAdd);
+            innerCache.Remove(toRemove);
+        });
+        // foreach (var addedGame in toAdd)
+        //     AvailableGames.Add(addedGame);
+        // foreach (var removedGame in toRemove)
+        //     AvailableGames.Remove(removedGame);
     }
 
     public void ResetSelections()
