a5c9eae8648b6725674bd9d5c59157c0aa5aa5c8
skrimix <28156185+skrimix@users.noreply.github.com>
2022-03-19 13:37:17 +0500

    Move InstalledGame class to it's own file

--- QSideloader/Models/Game.cs
+++ QSideloader/Models/Game.cs
@@ -64,27 +64,3 @@ public class Game : INotifyPropertyChanged
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
-
-public class InstalledGame : Game
-{
-    public InstalledGame(Game game, int installedVersionCode = -1)
-    {
-        GameName = game.GameName;
-        ReleaseName = game.ReleaseName;
-        PackageName = game.PackageName;
-        AvailableVersionCode = game.VersionCode;
-        VersionCode = game.VersionCode;
-        LastUpdated = game.LastUpdated;
-        GameSize = game.GameSize;
-        InstalledVersionCode = installedVersionCode;
-        IsUpdateAvailable = AvailableVersionCode > InstalledVersionCode;
-        UpdateStatus = IsUpdateAvailable
-            ? $"Update Available! ({InstalledVersionCode} -> {AvailableVersionCode})"
-            : "Up To Date";
-    }
-
-    [JsonIgnoreAttribute] public int InstalledVersionCode { get; set; }
-    [JsonIgnoreAttribute] public int AvailableVersionCode { get; set; }
-    [JsonIgnoreAttribute] public bool IsUpdateAvailable { get; set; }
-    [JsonIgnoreAttribute] public string UpdateStatus { get; set; }
-}
---
+++ QSideloader/Models/InstalledGame.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace QSideloader.Models;
+
+public class InstalledGame : Game
+{
+    public InstalledGame(Game game, int installedVersionCode = -1)
+    {
+        GameName = game.GameName;
+        ReleaseName = game.ReleaseName;
+        PackageName = game.PackageName;
+        AvailableVersionCode = game.VersionCode;
+        VersionCode = game.VersionCode;
+        LastUpdated = game.LastUpdated;
+        GameSize = game.GameSize;
+        InstalledVersionCode = installedVersionCode;
+        IsUpdateAvailable = AvailableVersionCode > InstalledVersionCode;
+        UpdateStatus = IsUpdateAvailable
+            ? $"Update Available! ({InstalledVersionCode} -> {AvailableVersionCode})"
+            : "Up To Date";
+    }
+
+    [JsonIgnore] public int InstalledVersionCode { get; set; }
+    [JsonIgnore] public int AvailableVersionCode { get; set; }
+    [JsonIgnore] public bool IsUpdateAvailable { get; set; }
+    [JsonIgnore] public string UpdateStatus { get; set; }
+}
