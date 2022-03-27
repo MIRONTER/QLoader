93661a077c1eddc8ac36a24c8024d7eb98243cf6
skrimix <28156185+skrimix@users.noreply.github.com>
2022-03-27 19:14:01 +0500

    Update metadata script

---
+++ QSideloader/scripts/updatemetadata.ps1
@@ -0,0 +1,18 @@
+$currentpath = Get-Location
+$mypath = $MyInvocation.MyCommand.Path
+$mypath = Split-Path $mypath -Parent
+Set-Location $mypath
+
+$mirrornumber = ( Get-Random -Minimum 1 -Maximum 20 ).ToString('00')
+
+Write-Output "Using mirror FFA-$mirrornumber"
+
+Write-Output "Updating thumbnails"
+..\tools\windows\rclone\FFA.exe copy --retries 1 --progress "FFA-${mirrornumber}:/Quest Games/.meta/thumbnails/" ../Resources/thumbnails/
+
+Write-Output "Updating videos"
+..\tools\windows\rclone\FFA.exe copy --retries 1 --progress "FFA-${mirrornumber}:/Quest Games/.meta/videos/" ../Resources/videos/
+
+Write-Output "Done!"
+
+Set-Location $currentpath
