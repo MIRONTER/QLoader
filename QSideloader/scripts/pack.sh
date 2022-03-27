bb7931be8d8a8d685ffb98fafd493423523fbf39
skrimix <28156185+skrimix@users.noreply.github.com>
2022-03-27 19:49:56 +0500

    Update packing script

--- QSideloader/scripts/pack.sh
+++ QSideloader/scripts/pack.sh
@@ -3,6 +3,7 @@
 echo "Deleting old archives"
 rm win-x64.zip
 rm linux-x64.tar.gz
+rm TrailersAddon.zip
 
 echo "Packing win-x64 build"
 zip -r win-x64.zip win-x64
@@ -16,3 +17,11 @@ echo "Packing osx-x64 build"
 chmod +x osx-x64/Loader
 chmod -R +x osx-x64/tools/
 zip -r osx-x64.zip osx-x64
+
+echo "Packing trailers addon"
+CURRENTDIR=$(pwd)
+cd ../../../
+zip -r $CURRENTDIR/TrailersAddon.zip Resources/videos/
+cd $CURRENTDIR
+
+echo "Done!"
