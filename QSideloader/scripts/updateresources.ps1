e9e6360ae9248331ae42de7e73b8ac0592cba886
skrimix <28156185+skrimix@users.noreply.github.com>
2022-03-31 14:24:38 +0500

    Update updateresources.ps1

--- QSideloader/scripts/updateresources.ps1
+++ QSideloader/scripts/updateresources.ps1
@@ -8,10 +8,10 @@ $mirrornumber = ( Get-Random -Minimum 1 -Maximum 20 ).ToString('00')
 Write-Output "Using mirror FFA-$mirrornumber"
 
 Write-Output "Updating thumbnails"
-..\tools\windows\rclone\FFA.exe copy --retries 1 --progress "FFA-${mirrornumber}:/Quest Games/.meta/thumbnails/" ../Resources/thumbnails/
+..\tools\windows\rclone\FFA.exe sync --retries 1 --progress "FFA-${mirrornumber}:/Quest Games/.meta/thumbnails/" ../Resources/thumbnails/
 
 Write-Output "Updating videos"
-..\tools\windows\rclone\FFA.exe copy --retries 1 --progress "FFA-${mirrornumber}:/Quest Games/.meta/videos/" ../Resources/videos/
+..\tools\windows\rclone\FFA.exe sync --retries 1 --progress "FFA-${mirrornumber}:/Quest Games/.meta/videos/" ../Resources/videos/
 
 Write-Output "Done!"
 
