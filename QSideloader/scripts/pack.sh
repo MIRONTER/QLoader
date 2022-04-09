7a46adb7997f79d2e72511abe097b27bba2a735c
skrimix <28156185+skrimix@users.noreply.github.com>
2022-04-09 19:10:40 +0500

    Don't build osx-arm64 for now

--- QSideloader/scripts/pack.sh
+++ QSideloader/scripts/pack.sh
@@ -4,7 +4,7 @@ echo "Deleting old archives"
 rm win-x64.zip
 rm linux-x64.tar.gz
 rm osx-x64.zip
-rm osx-arm64.zip
+#rm osx-arm64.zip
 rm TrailersAddon.zip
 
 echo "Packing win-x64 build"
@@ -20,20 +20,20 @@ chmod +x osx-x64/Loader
 chmod -R +x osx-x64/tools/
 zip -r osx-x64.zip osx-x64
 
-chmod +x osx-arm64/Loader
-chmod -R +x osx-arm64/tools/
-# Check if rcodesign tool is installed
-if ! [ -x "$HOME/.cargo/bin/rcodesign" ]; then
-  echo 'Error: rcodesign is not installed.' >&2
-  echo 'Please install apple-codesign rust crate.' >&2
-  exit 1
-fi
-# MacOS doesn't allow to run unsigned native M1 binaries
-echo "Signing osx-arm64 build with ad-hoc certificate"
-$HOME/.cargo/bin/rcodesign sign osx-arm64/Loader
-find osx-arm64 -name "*.dylib" -exec $HOME/.cargo/bin/rcodesign sign {} \;
-echo "Packing osx-arm64 build"
-zip -r osx-arm64.zip osx-arm64
+# chmod +x osx-arm64/Loader
+# chmod -R +x osx-arm64/tools/
+# # Check if rcodesign tool is installed
+# if ! [ -x "$HOME/.cargo/bin/rcodesign" ]; then
+#   echo 'Error: rcodesign is not installed.' >&2
+#   echo 'Please install apple-codesign rust crate.' >&2
+#   exit 1
+# fi
+# # MacOS doesn't allow to run unsigned native M1 binaries
+# echo "Signing osx-arm64 build with ad-hoc certificate"
+# $HOME/.cargo/bin/rcodesign sign osx-arm64/Loader
+# find osx-arm64 -name "*.dylib" -exec $HOME/.cargo/bin/rcodesign sign {} \;
+# echo "Packing osx-arm64 build"
+# zip -r osx-arm64.zip osx-arm64
 
 echo "Packing trailers addon"
 CURRENTDIR=$(pwd)
