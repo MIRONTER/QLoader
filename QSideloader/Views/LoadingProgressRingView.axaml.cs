d4e203320225b7eaf9a8841adddaa01ce2909b1f
skrimix <28156185+skrimix@users.noreply.github.com>
2022-06-29 01:34:33 +0500

    New loading overlay

---
+++ QSideloader/Views/LoadingProgressRingView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace QSideloader.Views;
+
+public partial class LoadingProgressRingView : UserControl
+{
+    public LoadingProgressRingView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
