9e88156a81d7a84cefa60c2b13488f6163657852
skrimix <28156185+skrimix@users.noreply.github.com>
2022-10-01 20:35:04 +0500

    Unhide debug console toggle

--- QSideloader/ViewModels/SideloaderSettingsViewModel.cs
+++ QSideloader/ViewModels/SideloaderSettingsViewModel.cs
@@ -116,11 +116,12 @@ public class SideloaderSettingsViewModel : ViewModelBase
     [Reactive] [JsonProperty] public string LastWirelessAdbHost { get; set; } = "";
     public string VersionString { get; } = Assembly.GetExecutingAssembly()
         .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "";
-#if DEBUG
+/*#if DEBUG
     public bool IsConsoleToggleable { get; }
 #else
     public bool IsConsoleToggleable { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-#endif
+#endif*/
+    public bool IsConsoleToggleable { get; } = true;
     [Reactive] public List<string> MirrorList { get; private set; } = new();
     [Reactive] public string? SelectedMirror { get; set; }
     public bool IsSwitchingMirror => _isSwitchingMirror.Value;
