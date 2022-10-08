ae29363c35ae1ce2c3f12afbd4c7f0efc0e47901
skrimix <28156185+skrimix@users.noreply.github.com>
2022-10-08 20:52:23 +0500

    Don't use fallback hwid

--- QSideloader/Utilities/GeneralUtils.cs
+++ QSideloader/Utilities/GeneralUtils.cs
@@ -95,12 +95,6 @@ public static class GeneralUtils
             throw;
         }
 
-        if (hwid is null)
-        {
-            Log.Warning("Failed to get HWID. Using InstallationId as fallback");
-            return GetHwidFallback();
-        }
-
         var bytes = Encoding.UTF8.GetBytes(hwid);
         var sha256 = SHA256.Create();
         var hash = sha256.ComputeHash(bytes);
