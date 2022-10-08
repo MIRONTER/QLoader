f9a8a1d139c3e453cc7019260bc6f69e6b596069
skrimix <28156185+skrimix@users.noreply.github.com>
2022-10-08 11:52:04 +0500

    Add GetHWID project

---
+++ GetHWID/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.InteropServices;
+using QSideloader.Utilities;
+
+var hwid = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? GeneralUtils.GetHwidCompat() : GeneralUtils.GetHwid();
+Console.WriteLine($"Your HWID: {hwid}\nPress any key to exit...");
+Console.ReadKey();
