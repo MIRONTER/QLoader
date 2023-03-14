1d2f352dd6b3ab85fb26abc174f27025752ad84a
skrimix <28156185+skrimix@users.noreply.github.com>
2023-03-14 14:53:22 +0500

    Request graceful exit from 7zip before killing it

--- QSideloader/Utilities/ZipUtil.cs
+++ QSideloader/Utilities/ZipUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using CliWrap;
@@ -18,9 +19,17 @@ public static class ZipUtil
             extractPath = Path.GetDirectoryName(archivePath);
         Log.Debug("Extracting archive: \"{ArchivePath}\" -> \"{ExtractPath}\"",
             archivePath, extractPath);
+        using var forcefulCts = new CancellationTokenSource();
+        // When the cancellation token is triggered,
+        // schedule forceful cancellation as a fallback.
+        await using var link = ct.Register(() =>
+            // ReSharper disable once AccessToDisposedClosure
+            forcefulCts.CancelAfter(TimeSpan.FromSeconds(3))
+        );
         await Cli.Wrap(PathHelper.SevenZipPath)
             .WithArguments($"x \"{archivePath}\" -y -aoa -o\"{extractPath}\"")
-            .ExecuteBufferedAsync(ct);
+            .ExecuteBufferedAsync(Console.OutputEncoding, Console.OutputEncoding,
+                forcefulCts.Token, ct);
     }
 
     public static void ExtractArchive(string archivePath, string? extractPath = null, CancellationToken ct = default)
@@ -38,12 +47,20 @@ public static class ZipUtil
         var archivePath = Path.Combine(destinationPath, archiveName);
         Log.Debug("Creating archive: \"{SourcePath}\" -> \"{ArchivePath}\"",
             sourcePath, archivePath);
+        using var forcefulCts = new CancellationTokenSource();
+        // When the cancellation token is triggered,
+        // schedule forceful cancellation as a fallback.
+        await using var link = ct.Register(() =>
+            // ReSharper disable once AccessToDisposedClosure
+            forcefulCts.CancelAfter(TimeSpan.FromSeconds(3))
+        );
         if (File.Exists(archivePath))
             File.Delete(archivePath);
         sourcePath = Path.GetFullPath(sourcePath) + Path.DirectorySeparatorChar;
         await Cli.Wrap(PathHelper.SevenZipPath)
             .WithArguments($"a -mx1 -aoa \"{archivePath}\" \"{sourcePath}*\"")
-            .ExecuteBufferedAsync(ct);
+            .ExecuteBufferedAsync(Console.OutputEncoding, Console.OutputEncoding,
+                forcefulCts.Token, ct);
         return archivePath;
     }
 }
