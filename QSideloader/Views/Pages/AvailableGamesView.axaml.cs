dd9b458cf83063df4700d4a14d3d29d8ac877aa2
skrimix <28156185+skrimix@users.noreply.github.com>
2023-10-31 00:53:35 +0500

    Change a few comments

--- QSideloader/Views/Pages/AvailableGamesView.axaml.cs
+++ QSideloader/Views/Pages/AvailableGamesView.axaml.cs
@@ -45,32 +45,31 @@ public partial class AvailableGamesView : ReactiveUserControl<AvailableGamesView
         {
             switch (e.Key)
             {
-                // If Enter or arrow down/up is pressed, focus the data grid
+                // Enter or arrow down/up - focus the data grid
                 case Key.Down or Key.Up or Key.Enter:
-                    var isDataGridFocused = dataGrid.IsFocused;
-                    if (!isDataGridFocused)
+                    if (!dataGrid.IsFocused)
                     {
                         dataGrid.Focus();
                         dataGrid.SelectedIndex = 0;
                     }
 
                     break;
-                // If Escape is pressed clear the search box
+                // Escape - clear the search box
                 case Key.Escape:
                     SearchBox.Text = "";
                     break;
-                // If Space is pressed, toggle the selected game's selected state
+                // Space - toggle the highlighted game's selected state
                 case Key.Space:
                     if (selectedGame is null) return;
                     selectedGame.IsSelected = !selectedGame.IsSelected;
                     break;
-                // If Alt is pressed, show game details for the selected game
+                // Alt - show game details for the highlighted game
                 case Key.LeftAlt or Key.RightAlt:
                     if (selectedGame is null) return;
                     Globals.MainWindowViewModel!.ShowGameDetails.Execute(selectedGame)
                         .Subscribe(_ => { }, _ => { });
                     break;
-                // If F5 is pressed, refresh the list
+                // F5 - refresh the list
                 case Key.F5:
                     ViewModel!.Refresh.Execute(true).Subscribe(_ => { }, _ => { });
                     break;
@@ -87,11 +86,11 @@ public partial class AvailableGamesView : ReactiveUserControl<AvailableGamesView
         }
         else
         {
-            // If Ctrl+F is pressed, focus the search box
+            // Ctrl+F - focus the search box
             if (e is {KeyModifiers: KeyModifiers.Control, Key: Key.F})
             {
-                // Highlight the text
                 SearchBox.Focus();
+                // Highlight all text
                 SearchBox.SelectionStart = 0;
                 SearchBox.SelectionEnd = SearchBox.Text?.Length ?? 0;
                 e.Handled = true;
