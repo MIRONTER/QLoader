8dee6e8b120df1fd2afed7ddd13853fd74b4afab
skrimix <28156185+skrimix@users.noreply.github.com>
2023-12-08 01:19:37 +0500

    Add a hack for ProgressRing
    Sometimes ProgressRing doesn't get properly disabled which causes constant window rendering. We can create it in code-behind and remove completely when it's not needed

--- QSideloader/Views/LoadingProgressRingView.axaml.cs
+++ QSideloader/Views/LoadingProgressRingView.axaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using FluentAvalonia.UI.Controls;
+using SerilogTimings;
 
 namespace QSideloader.Views;
 
@@ -7,5 +11,30 @@ public partial class LoadingProgressRingView : UserControl
     public LoadingProgressRingView()
     {
         InitializeComponent();
+        IsVisibleProperty.Changed.AddClassHandler<LoadingProgressRingView>(OnIsVisibleChanged);
+    }
+    
+    private void OnIsVisibleChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        // HACK: This is needed because the progress ring refuses to disable sometimes and causes constant rendering
+        if (e.NewValue is not bool isVisible)
+            return;
+        if (isVisible)
+        {
+            var progressRing = new ProgressRing
+            {
+                Height = 55,
+                Margin = new Thickness(0, 20, 0, 0),
+                IsEnabled = true,
+                IsIndeterminate = true,
+                Name = "LoadingProgressRing"
+            };
+            StackPanel.Children.Insert(0, progressRing);
+        }
+        else
+        {
+            var progressRing = StackPanel.Children.FirstOrDefault(x => x.Name == "LoadingProgressRing");
+            if (progressRing != null) StackPanel.Children.Remove(progressRing);
+        }
     }
 }
