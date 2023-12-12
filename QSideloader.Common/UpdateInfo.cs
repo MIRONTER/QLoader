071869d84c9b1987b708ea6b92093e14e9e664a7
skrimix <28156185+skrimix@users.noreply.github.com>
2023-12-13 02:21:41 +0500

    Update UpdateInfo

--- QSideloader.Common/UpdateInfo.cs
+++ QSideloader.Common/UpdateInfo.cs
@@ -9,22 +9,35 @@ namespace QSideloader.Common;
 
 public class UpdateInfo
 {
-    // private const string UpdateUrl = "https://qloader.5698452.xyz/api/v1/update_info";
-    private const string UpdateUrl = "https://qloader.5698452.xyz/files/update_info.json"; // For testing
-    [JsonPropertyName("version")] public Version Version { get; set; } = new();
-    [JsonPropertyName("versionString")] public string VersionString { get; set; } = "";
-    [JsonPropertyName("downloadUrl")] public string DownloadUrl { get; set; } = "";
-    [JsonPropertyName("checksum")] public string Checksum { get; set; } = "";
-    [JsonPropertyName("releaseNotes")] public string ReleaseNotes { get; set; } = "";
-    [JsonPropertyName("channel")] public string Channel { get; set; } = "";
+    private const string UpdateInfoUrl = "https://qloader.5698452.xyz/files/updater/update_info.json";
     
-    public static async Task<List<UpdateInfo>> GetUpdatesAsync()
+    [JsonPropertyName("versions")] public List<VersionItem> VersionList { get; set; } = new();
+    [JsonPropertyName("updater")] public List<UpdateAsset> Updater { get; set; } = new();
+    
+    public static async Task<UpdateInfo> GetInfoAsync()
     {
         using var client = new HttpClient();
-        var response = await client.GetAsync(UpdateUrl);
+        var response = await client.GetAsync(UpdateInfoUrl);
         response.EnsureSuccessStatusCode();
         var json = await response.Content.ReadAsStringAsync();
-        var updateInfo = JsonSerializer.Deserialize(json, CommonJsonSerializerContext.Default.ListUpdateInfo);
+        var updateInfo = JsonSerializer.Deserialize(json, CommonJsonSerializerContext.Default.UpdateInfo);
         return updateInfo ?? throw new Exception("Failed to deserialize update info list");
     }
+}
+
+public class VersionItem
+{
+    [JsonPropertyName("channel")] public string Channel { get; set; } = "";
+    [JsonPropertyName("versionString")] public string VersionString { get; set; } = "";
+    [JsonPropertyName("version")] public Version Version { get; set; } = new();
+    [JsonPropertyName("assets")] public List<UpdateAsset> Assets { get; set; } = [];
+    [JsonPropertyName("releaseNotes")] public string ReleaseNotes { get; set; } = "";
+}
+
+
+public class UpdateAsset
+{
+    [JsonPropertyName("os")] public string Os { get; set; } = "";
+    [JsonPropertyName("url")] public string Url { get; set; } = "";
+    [JsonPropertyName("sha256")] public string Sha256 { get; set; } = "";
 }
